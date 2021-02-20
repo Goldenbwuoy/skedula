17147fd0e87839eca288394f038460a370ed776c
Goldenbwuoy <mumanikidzwag@gmail.com>
2021-02-20 18:16:38 +0800

    connect to a cloud mongodb database

--- server.js
+++ server.js
@@ -3,23 +3,42 @@ const app = require("./express");
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(
-  process.env.MONGODB_LOCAL_URI,
-  {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  },
-  () => console.log("Connected to mongo")
-);
-mongoose.connection.on("error", () => {
-  throw new Error(
-    `Unable to connect to database: ${process.env.MONGODB_LOCAL_URI}`
-  );
-});
+
+const connectMongo = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_REMOTE_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+    console.log("Connected to mongo");
+  } catch (err) {
+    console.log(err);
+  }
+};
+connectMongo();
+// mongoose.connect(
+//   process.env.MONGODB_REMOTE_URI,
+//   {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//   },
+//   () => console.log("Connected to mongo")
+// );
+// mongoose.connection.on("error", () => {
+//   throw new Error(
+//     `Unable to connect to database: ${process.env.MONGODB_REMOTE_URI}`
+//   );
+// });
 
 const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello there");
+});
 
 app.listen(PORT, (err) => {
   if (err) {
