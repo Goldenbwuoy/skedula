07db2f8f949f719f76f1fe5126ee7516d8f97a32
Goldenbwuoy <mumanikidzwag@gmail.com>
2021-02-22 13:40:42 +0800

    validate user data before signing up

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,7 +2,21 @@ const User = require("../models/user.model");
 const errorHandler = require("../helpers/dbErrorHandler");
 
 const create = async (req, res, next) => {
-  const { email, password, role } = req.body;
+  const {
+    firstName,
+    lastName,
+    email,
+    phoneNumber,
+    practice_number,
+    password,
+    role,
+  } = req.body;
+  if (!firstName || !lastName || !email || !phoneNumber || !password) {
+    return res.status(400).json({ error: "Please fill in all fields!" });
+  }
+  if (role == "Doctor" && !practice_number) {
+    return res.status(400).json({ error: "Practice Number is required" });
+  }
   const user = new User({ email, password, role });
   try {
     const savedUser = await user.save();
